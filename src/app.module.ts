5496a1ef2cb70d4690e2d08e20bd9e09b6050c1c
yeshu10 <yeshvigupta10@gmail.com>
2024-12-02 23:51:19 +0530

    initialcommit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [AuthModule, UsersModule],
+  controllers: [],
+  providers: [PrismaService],
+})
+export class AppModule {}
